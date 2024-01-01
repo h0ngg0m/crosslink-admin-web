cc7cf4656346de4166a92d3a269dfa2829861173
honggom <jaehongbyeon98@gmail.com>
2024-01-01 17:39:47 +0900

    :sparkles: 박스 추가 폼

---
+++ src/views/management/box/table/BoxAddSheet.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-bottom-sheet :inset="true">
+    <template v-slot:activator="{ props }">
+      <slot :props="props" />
+    </template>
+    <v-card title="박스 추가">
+      <div class="pa-3">
+        <TextField v-model="name" label="이름" density="compact" />
+        <TextArea v-model="description" label="설명" density="compact" />
+        <TextField v-model="address" label="주소" density="compact" />
+        <SingleSelect
+          v-model="city"
+          label="도시"
+          :items="Object.values(City)"
+          :chips="false"
+          :hide-details="false"
+        />
+        <TextField v-model="tel" label="연락처" density="compact" />
+      </div>
+    </v-card>
+  </v-bottom-sheet>
+</template>
+<script setup lang="ts">
+import TextField from '@/components/TextField.vue'
+import { ref } from 'vue'
+import SingleSelect from '@/components/SingleSelect.vue'
+import { City } from '@/definitions/enums'
+import TextArea from '@/components/TextArea.vue'
+
+const name = ref('')
+const address = ref('')
+const description = ref('')
+const city = ref<string | null>(null)
+const tel = ref('')
+</script>
--- src/views/management/box/table/BoxTableHeader.vue
+++ src/views/management/box/table/BoxTableHeader.vue
@@ -1,3 +1,28 @@
 <template>
-  <v-card height="120" variant="plain"> </v-card>
+  <v-card height="200" variant="plain" class="d-flex flex-column pl-3 pr-3">
+    <div class="h-50 d-flex mt-3">
+      <p class="text-h6">박스 관리</p>
+      <BoxAddSheet #default="{ props }">
+        <v-btn
+          v-bind="props"
+          class="ml-auto"
+          prepend-icon="mdi-package-variant-closed-plus"
+          variant="outlined"
+          color="green-lighten-1"
+          >박스 추가</v-btn
+        >
+      </BoxAddSheet>
+    </div>
+    <div class="h-50">
+      <MultiSelect v-model="selectedCity" label="도시" :items="Object.values(City)" />
+    </div>
+  </v-card>
 </template>
+<script setup lang="ts">
+import MultiSelect from '@/components/MultiSelect.vue'
+import { City } from '@/definitions/enums'
+import { ref } from 'vue'
+import BoxAddSheet from '@/views/management/box/table/BoxAddSheet.vue'
+
+const selectedCity = ref<string[]>([])
+</script>
